c4870c46efd165ddd471de91b3198a693358220e
KeyboardFire <andy@keyboardfire.com>
2014-12-11 19:43:28 -0600

    option parsing started

--- lib/kevcode.rb
+++ lib/kevcode.rb
@@ -1,3 +1,18 @@
 #!/usr/bin/ruby
 
-puts 'hello'
+require 'optparse'
+
+opts = {}
+OptionParser.new {|o|
+    o.banner = 'Usage: ./kevcode.rb [options]'
+
+    o.on('-e', '--exec', 'Execute a string passed as an argument') {|e|
+        opts[:exec] = e
+    }
+}.parse!
+
+if opts[:exec]
+    # TODO
+else
+    # TODO
+end
