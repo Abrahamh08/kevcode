ced966febdb1aef3205e1b2bc2d241a1980bb593
KeyboardFire <andy@keyboardfire.com>
2014-12-11 19:50:20 -0600

    fixed a small bug

--- lib/kevcode.rb
+++ lib/kevcode.rb
@@ -17,14 +17,20 @@ if __FILE__ == $0
     OptionParser.new {|o|
         o.banner = 'Usage: ./kevcode.rb [options]'
 
-        o.on('-e', '--exec', 'Execute a string passed as an argument') {|e|
+        o.on('-e', '--exec code', 'Execute a string passed as an argument') {|e|
             opts[:exec] = e
+        }
+
+        o.on('-i', '--interactive', 'Enter an interactive REPL instead') {|i|
+            opts[:interactive] = i
         }
     }.parse!
 
     k = KevCode.new
     if opts[:exec]
         k.run opts[:exec]
+    elsif opts[:interactive]
+        # TODO REPL
     else
         k.run STDIN.read
     end
